d8139f05d60773abb8f5c8d22eb3e3576fd6ede2
Trần Minh <tranminh1236@gmail.com>
2021-09-04 16:09:38 +0700

    update header fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,3 +7,15 @@
         Hello <?php echo $name; ?>
     </h1>
 @stop
+
+@section('js')
+<script>
+    $(window).scroll(function(){
+        var sticky = $('header'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+</script>
+@stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/layout.css" rel="stylesheet">
+        @yield('style')
     </head>
-    <body>
+    <body style="height: 1000px">
         @include('layouts.header')
 
         <div class="container">
@@ -16,6 +17,8 @@
         @section('layouts.footer')
         @show
 
+        <script src="js/jquery.min.js"></script>
+        @yield('js')
         <script src="js/bootstrap.bundle.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
