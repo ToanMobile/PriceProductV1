b21f34a3ebd8ff8d12f91fb3b2ea13a48006561e
Trần Minh <tranminh1236@gmail.com>
2021-09-05 13:00:14 +0700

    update keepstate choose platform

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,23 +7,23 @@
         <section class="banner">
             <img src="img/BannerBg.svg" width="100%" />
             <div class="platform">
-                <div class="platform-item">
+                <div class="platform-item platform-active" data-platform="web">
                     <img src="img/iconWeb.svg" />
                     <p>Website</p>
                 </div>
-                <div class="platform-item">
+                <div class="platform-item" data-platform="desktop">
                     <img src="img/iconDesktop.svg" />
                     <p>Desktop</p>
                 </div>
-                <div class="platform-item">
+                <div class="platform-item" data-platform="tablet">
                     <img src="img/iconTablet.svg" />
                     <p>Tablet</p>
                 </div>
-                <div class="platform-item">
+                <div class="platform-item" data-platform="mobile">
                     <img src="img/iconMobile.svg" />
                     <p>Mobile</p>
                 </div>
-                <div class="platform-item">
+                <div class="platform-item" data-platform="orther">
                     <img src="img/iconOrther.svg" />
                     <p>Khác</p>
                 </div>
@@ -41,10 +41,12 @@
             </div>
         </section>--}}
     </div>
+    <input type="hidden" id="platformChoose" value="" />
 @stop
 
 @section('js')
 <script>
+    var arrPlatform = ['web'];
     $(window).scroll(function(){
         var sticky = $('header'),
             scroll = $(window).scrollTop();
@@ -53,11 +55,18 @@
         else sticky.removeClass('fixed');
     });
     $('.platform-item').click(function () {
-        if ($(this).hasClass('platform-active')) {
-            $(this).removeClass('platform-active');
+        let platformObj = $(this);
+        if (platformObj.hasClass('platform-active')) {
+            platformObj.removeClass('platform-active');
+            var index = arrPlatform.indexOf(platformObj.attr('data-platform'));
+            if (index !== -1) {
+                arrPlatform.splice(index, 1);
+            }
         } else {
-            $(this).addClass('platform-active');
+            platformObj.addClass('platform-active');
+            arrPlatform.push(platformObj.attr('data-platform'));
         }
+        $('#platformChoose').val(arrPlatform.join(','));
     });
 </script>
 @stop
