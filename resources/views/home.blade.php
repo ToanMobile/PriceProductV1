090890a25450a6cd3b2baa191b1f9959530debb0
Trần Minh <tranminh1236@gmail.com>
2021-09-07 17:05:22 +0700

    update bảng giá

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,6 +28,7 @@
                     <p>Khác</p>
                 </div>
             </div>
+            <input type="hidden" id="platformChoose" value="" />
         </section>
         {{--<section class="banner">
             <img src="img/pngegg 1.png" class="component1" />
@@ -40,8 +41,212 @@
                 </div>
             </div>
         </section>--}}
+        <section class="package">
+            <div class="package-popular">
+                <h3>PHỔ BIẾN NHẤT</h3>
+            </div>
+            <div class="package-price">
+                <div class="package-content">
+                    <div class="package-header">CƠ BẢN</div>
+                    <div class="package-intro">
+                        <div class="package-detail">
+                            <div class="price-info">
+                                <div class="price-sale-off">
+                                    <span>5,979,000 VNĐ</span>
+                                    <div class="sale-off-percent">
+                                        <span>TIẾT KIỆM 77%</span>
+                                    </div>
+                                </div>
+                                <div class="price">
+                                    <span><span>đ</span> 99,000</span>
+                                </div>
+                            </div>
+                            <span>Giao diện hiện đại, đa dạng, sạch theo trend thiết kế mới nhất.</span>
+                        </div>
+                        <div class="package-benefit">
+                            <h3>Bao Gồm</h3>
+                            <div class="benefit-detail">
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>1 Section</span>
+                                </div>
+                                <div class="benefit">
+                                    <img src="img/BooleanFail.svg" />
+                                    <span>Hiệu Ứng Tương Tác Prototype</span>
+                                </div>
+                                <div class="benefit">
+                                    <img src="img/BooleanFail.svg" />
+                                    <span>Giám sát tiến độ thực tế</span>
+                                </div>
+                                <div class="benefit">
+                                    <img src="img/BooleanFail.svg" />
+                                    <span>Source File Cơ Bản</span>
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="add-section">
+                                <span>Thêm số lượng Section</span>
+                                <div class="tool-add">
+                                    <button type="button" class="reduce"><img src="img/reduce.svg"></button>
+                                    <span>1</span>
+                                    <button type="button" class="increase"><img src="img/increase.svg" /></button>
+                                </div>
+                                <span class="price-per-page"><b>$14.99 </b> / Page</span>
+                            </div>
+                            <hr/>
+                            <div class="timeline">
+                                <div class="time">
+                                    <img src="img/IconTimeline.svg" />
+                                    <span><b>2</b> Ngày</span>
+                                </div>
+                                <div class="edit">
+                                    <img src="img/IconEdit.svg" />
+                                    <span><b>1</b> Chỉnh Sửa</span>
+                                </div>
+                            </div>
+                            <hr class="hidden-footer"/>
+                            <div class="package-footer hidden-footer">
+                                <button type="button" class="contact">TƯ VẤN TRỌN GÓI</button>
+                                <button type="button" class="payment">CỌC TRƯỚC 50%</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="package-content" style="border-right:0; border-left:0;">
+                    <div class="package-header">NÂNG CAO</div>
+                    <div class="package-intro">
+                        <div class="package-detail">
+                            <div class="price-info">
+                                <div class="price-sale-off">
+                                    <span>5,979,000 VNĐ</span>
+                                    <div class="sale-off-percent" style="background-color: #FF5E61;">
+                                        <span>TIẾT KIỆM 77%</span>
+                                    </div>
+                                </div>
+                                <div class="price">
+                                    <span><span>đ</span> 2,999,000</span>
+                                </div>
+                            </div>
+                            <span>Giao diện hiện đại, đa dạng, sạch theo trend thiết kế mới nhất.</span>
+                        </div>
+                        <div class="package-benefit">
+                            <h3>What’s included</h3>
+                            <div class="benefit-detail">
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>8 Trang/Màn Hình</span>
+                                </div>
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>Hiệu Ứng Tương Tác Prototype</span>
+                                </div>
+                                <div class="benefit">
+                                    <img src="img/BooleanFail.svg" />
+                                    <span>Giám sát tiến độ thực tế</span>
+                                </div>
+                                <div class="benefit">
+                                    <img src="img/BooleanFail.svg" />
+                                    <span>Source File Cơ Bản</span>
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="add-section">
+                                <span>Thêm số Trang/Màn Hình</span>
+                                <div class="tool-add">
+                                    <button type="button" class="reduce"><img src="img/reduce.svg"></button>
+                                    <span>5</span>
+                                    <button type="button" class="increase"><img src="img/increase.svg" /></button>
+                                </div>
+                                <span class="price-per-page"><b>$14.99 </b> / Page</span>
+                            </div>
+                            <hr/>
+                            <div class="timeline">
+                                <div class="time">
+                                    <img src="img/IconTimeline.svg" />
+                                    <span><b>3</b> Ngày</span>
+                                </div>
+                                <div class="edit">
+                                    <img src="img/IconEdit.svg" />
+                                    <span><b>1</b> Chỉnh Sửa</span>
+                                </div>
+                            </div>
+                            <hr class="hidden-footer"/>
+                            <div class="package-footer hidden-footer">
+                                <button type="button" class="contact">TƯ VẤN TRỌN GÓI</button>
+                                <button type="button" class="payment">CỌC TRƯỚC 50%</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="package-content">
+                    <div class="package-header">CHUYÊN NGHIỆP</div>
+                    <div class="package-intro">
+                        <div class="package-detail">
+                            <div class="price-info">
+                                <div class="price-sale-off">
+                                    <span>5,979,000 VNĐ</span>
+                                    <div class="sale-off-percent">
+                                        <span>TIẾT KIỆM 78%</span>
+                                    </div>
+                                </div>
+                                <div class="price">
+                                    <span><span>đ</span> 3,999,000</span>
+                                </div>
+                            </div>
+                            <span>Giao diện hiện đại, đa dạng, sạch theo trend thiết kế mới nhất.</span>
+                        </div>
+                        <div class="package-benefit">
+                            <h3>Trọn gói</h3>
+                            <div class="benefit-detail">
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>8 Trang/Màn Hình</span>
+                                </div>
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>Hiệu Ứng Tương Tác Prototype</span>
+                                </div>
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>Giám sát tiến độ thực tế</span>
+                                </div>
+                                <div class="benefit active">
+                                    <img src="img/BooleanTrue.svg" />
+                                    <span>Source File Cơ Bản</span>
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="add-section">
+                                <span>Thêm số Trang/Màn Hình</span>
+                                <div class="tool-add">
+                                    <button type="button" class="reduce"><img src="img/reduce.svg"></button>
+                                    <span>8</span>
+                                    <button type="button" class="increase"><img src="img/increase.svg" /></button>
+                                </div>
+                                <span class="price-per-page"><b>$14.99 </b> / Page</span>
+                            </div>
+                            <hr/>
+                            <div class="timeline">
+                                <div class="time">
+                                    <img src="img/IconTimeline.svg" />
+                                    <span><b>8</b> Ngày</span>
+                                </div>
+                                <div class="edit">
+                                    <img src="img/IconEdit.svg" />
+                                    <span><b>Không Giới Hạn</b> Sửa</span>
+                                </div>
+                            </div>
+                            <hr class="hidden-footer"/>
+                            <div class="package-footer hidden-footer">
+                                <button type="button" class="contact">TƯ VẤN TRỌN GÓI</button>
+                                <button type="button" class="payment">CỌC TRƯỚC 50%</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-    <input type="hidden" id="platformChoose" value="" />
 @stop
 
 @section('js')
